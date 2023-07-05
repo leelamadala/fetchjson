716930fc75bb042fe30fb56fbeed6283b0ab47ba
leelamadala <mlsankar@icloud.com>
2023-07-05 12:03:45 +0530

    using types

--- index.ts
+++ index.ts
@@ -27,10 +27,12 @@ axios.get(url).then(response =>{
         Has a title of: ${title}
         Is it finished? ${completed}
     `);
+    
+    logTodo(id, title, completed);
     }
 );
 
-const logTodo =(id, title, completed) =>{
+const logTodo =(id: number, title: string, completed: boolean) =>{
     console.log(`
         The Todo with ID: ${id}
         Has a title of: ${title}
